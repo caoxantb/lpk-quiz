3b67da80e856080dd7bd2a0d59a850f96337ebe2
Xuan-An Cao <an.cao@tuni.fi>
2025-11-11 06:17:43 +0200

    test: config based on vercel documentation

--- server/index.js
+++ server/index.js
@@ -1,13 +1,3 @@
-import http from "http";
 import app from "./app.js";
 
-const initServer = () => {
-  const server = http.createServer(app);
-  const PORT = process.env.PORT || 8080;
-
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-initServer();
+export default app;
