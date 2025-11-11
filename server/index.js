0bf9ef5d506aa96b409a71bbfefff4a6404c1b06
Xuan-An Cao <an.cao@tuni.fi>
2025-11-11 06:31:25 +0200

    fix: vercel config

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
+import http from "http";
 import app from "./app.js";
 
-export default app;
+const initServer = () => {
+  const server = http.createServer(app);
+  const PORT = process.env.PORT || 8080;
+
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+initServer();
